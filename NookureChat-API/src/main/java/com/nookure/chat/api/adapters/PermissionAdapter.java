248ec88cb64d4b0ccaef2ca882f4d184a447cb03
Angelillo15 <contact@angelillo15.es>
2023-12-24 20:41:16 +0100

    Create permission adapter

---
+++ NookureChat-API/src/main/java/com/nookure/chat/api/adapters/PermissionAdapter.java
@@ -0,0 +1,9 @@
+package com.nookure.chat.api.adapters;
+
+import org.bukkit.entity.Player;
+
+public abstract class PermissionAdapter {
+  abstract public String getHighestGroup(Player player);
+
+  abstract public String[] getGroups(Player player);
+}
