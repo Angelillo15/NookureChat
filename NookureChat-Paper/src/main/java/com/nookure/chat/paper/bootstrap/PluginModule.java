77ef403723463998fd53ce3c940226cd7930220f
Angelillo15 <contact@angelillo15.es>
2023-12-24 20:40:57 +0100

    Create plugin module

--- NookureChat-Paper/src/main/java/com/nookure/chat/paper/bootstrap/PluginModule.java
+++ NookureChat-Paper/src/main/java/com/nookure/chat/paper/bootstrap/PluginModule.java
@@ -1,12 +1,26 @@
 package com.nookure.chat.paper.bootstrap;
 
 import com.google.inject.AbstractModule;
+import com.nookure.chat.api.Logger;
+import com.nookure.chat.api.NookureChat;
+import com.nookure.chat.api.adapters.NoImplPermissionAdapter;
+import com.nookure.chat.api.adapters.PermissionAdapter;
+import com.nookure.chat.api.adapters.VaultPermissionAdapter;
+import com.nookure.chat.api.config.Config;
+import com.nookure.chat.api.config.ConfigMapper;
+import com.nookure.chat.api.config.FormatConfig;
+import com.nookure.chat.api.managers.FilterManager;
+import net.kyori.adventure.platform.AudienceProvider;
+import net.milkbowl.vault.permission.Permission;
+import org.bukkit.Bukkit;
+import org.bukkit.plugin.RegisteredServiceProvider;
 import org.bukkit.plugin.java.JavaPlugin;
 
 public class PluginModule extends AbstractModule {
-  private final JavaPlugin plugin;
+  private final ChatBootstrapper plugin;
+
   public PluginModule(
-      JavaPlugin plugin
+      ChatBootstrapper plugin
   ) {
     this.plugin = plugin;
   }
@@ -14,5 +28,29 @@ public class PluginModule extends AbstractModule {
   @Override
   protected void configure() {
     bind(JavaPlugin.class).toInstance(plugin);
+    bind(Logger.class).toInstance(plugin.getPLogger());
+    bind(ChatBootstrapper.class).toInstance(plugin);
+    bind(NookureChat.class).toInstance(plugin);
+    bind(AudienceProvider.class).toInstance(plugin.getAudiences());
+    bind(ConfigMapper.class).toInstance(plugin.getConfigMapper());
+    bind(Config.class).toInstance(plugin.getPluginConfig().get());
+    bind(FormatConfig.class).toInstance(plugin.getFormatConfig().get());
+    bind(FilterManager.class).asEagerSingleton();
+    bind(PermissionAdapter.class).toInstance(getPermissionAdapter());
+    bind(ChatBootstrapper.class).toInstance(plugin);
+  }
+
+  public PermissionAdapter getPermissionAdapter() {
+    try {
+      Class.forName("net.milkbowl.vault.permission.Permission");
+      return new VaultPermissionAdapter(setupPermissions());
+    } catch (ClassNotFoundException e) {
+      return new NoImplPermissionAdapter();
+    }
+  }
+
+  private Permission setupPermissions() {
+    RegisteredServiceProvider<Permission> rsp = Bukkit.getServer().getServicesManager().getRegistration(Permission.class);
+    return rsp != null ? rsp.getProvider() : null;
   }
 }
