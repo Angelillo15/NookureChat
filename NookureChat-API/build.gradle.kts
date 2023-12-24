0e68fd868cc5f472ee3f4f88d43dffdf72d982a5
Angelillo15 <contact@angelillo15.es>
2023-12-24 20:38:54 +0100

    Add dependencies to api

--- NookureChat-API/build.gradle.kts
+++ NookureChat-API/build.gradle.kts
@@ -1,18 +1,24 @@
 plugins {
-    id("java")
-}
-
-group = "com.nookure.chat"
-
-repositories {
-    mavenCentral()
+  id("net.kyori.blossom") version "2.1.0"
 }
 
 dependencies {
-    testImplementation(platform("org.junit:junit-bom:5.9.1"))
-    testImplementation("org.junit.jupiter:junit-jupiter")
+  compileOnly(libs.paperApi)
+  compileOnly(libs.adventureBukkit)
+  compileOnly(libs.adventureLegacy)
+  compileOnly(libs.adventureApi)
+  compileOnly(libs.miniMessage)
+  compileOnly(libs.configurateYaml)
+  compileOnly(libs.vault)
+  compileOnly(libs.placeholderApi)
 }
 
-tasks.test {
-    useJUnitPlatform()
+sourceSets {
+  main {
+    blossom {
+      javaSources {
+        property("version", rootProject.version.toString())
+      }
+    }
+  }
 }
