c6979ab656b320b15fd444e3c94fa32beb156495
Angelillo15 <contact@angelillo15.es>
2023-12-24 20:43:39 +0100

    Create FloodConfig

---
+++ NookureChat-API/src/main/java/com/nookure/chat/api/config/partials/FloodConfig.java
@@ -0,0 +1,28 @@
+package com.nookure.chat.api.config.partials;
+
+import org.spongepowered.configurate.objectmapping.ConfigSerializable;
+import org.spongepowered.configurate.objectmapping.meta.Setting;
+
+@ConfigSerializable
+public class FloodConfig extends FilterConfig {
+  @Setting
+  private boolean enabled = true;
+  @Setting
+  private int maxRepetitions = 5;
+  @Setting
+  private String denyMessage = "{prefix} <red>Doing flood isn't allowed!</red>";
+
+  public int getMaxRepetitions() {
+    return maxRepetitions - 1;
+  }
+
+  @Override
+  public boolean isEnabled() {
+    return enabled;
+  }
+
+  @Override
+  public String getDenyMessage() {
+    return denyMessage;
+  }
+}
