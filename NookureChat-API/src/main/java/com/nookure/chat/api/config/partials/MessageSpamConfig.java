a1bc9a5a736b2dba558f13fc3f23901a9df775d8
Angelillo15 <contact@angelillo15.es>
2023-12-29 21:48:39 +0100

    Create MessageSpam Filter

---
+++ NookureChat-API/src/main/java/com/nookure/chat/api/config/partials/MessageSpamConfig.java
@@ -0,0 +1,27 @@
+package com.nookure.chat.api.config.partials;
+
+import org.spongepowered.configurate.objectmapping.ConfigSerializable;
+import org.spongepowered.configurate.objectmapping.meta.Setting;
+
+@ConfigSerializable
+public class MessageSpamConfig extends FilterConfig {
+  @Setting
+  private boolean enabled = true;
+  @Setting
+  private int cooldown = 3;
+  @Setting
+  private String denyMessage = "<red>You are sending messages too fast! Please wait {time} seconds before sending another message.";
+  @Override
+  public boolean isEnabled() {
+    return enabled;
+  }
+
+  @Override
+  public String getDenyMessage() {
+    return denyMessage;
+  }
+
+  public int getCooldown() {
+    return cooldown;
+  }
+}
---
+++ NookureChat-Paper/src/main/java/com/nookure/chat/paper/filter/MessageSpamFilter.java
@@ -0,0 +1,45 @@
+package com.nookure.chat.paper.filter;
+
+import com.google.inject.Inject;
+import com.nookure.chat.api.chat.ChatFilter;
+import com.nookure.chat.api.chat.ChatFilterData;
+import com.nookure.chat.api.config.Config;
+import com.nookure.chat.api.config.ConfigurationContainer;
+import org.bukkit.entity.Player;
+import org.jetbrains.annotations.NotNull;
+
+import java.util.HashMap;
+import java.util.UUID;
+
+import static com.nookure.chat.paper.utils.MessageUtils.sendMessage;
+
+@ChatFilterData(
+    name = "messageSpam",
+    permission = "nookurechat.bypass.filter.messageSpam"
+)
+public class MessageSpamFilter extends ChatFilter {
+  @Inject
+  private ConfigurationContainer<Config> config;
+  private final HashMap<UUID, Long> lastMessage = new HashMap<>();
+  @Override
+  public boolean check(@NotNull Player player, @NotNull String message) {
+    if (!lastMessage.containsKey(player.getUniqueId())) {
+      lastMessage.put(player.getUniqueId(), System.currentTimeMillis());
+      return true;
+    }
+
+    long lastMessageTime = lastMessage.get(player.getUniqueId());
+    long currentTime = System.currentTimeMillis();
+    long difference = currentTime - lastMessageTime;
+
+    if (difference < config.get().filters.messageSpam.getCooldown() * 1000L) {
+      sendMessage(player, config.get().filters.messageSpam.getDenyMessage().replace(
+          "{time}", String.valueOf(config.get().filters.messageSpam.getCooldown() - (difference / 1000)))
+      );
+      return false;
+    }
+
+    lastMessage.put(player.getUniqueId(), currentTime);
+    return true;
+  }
+}
