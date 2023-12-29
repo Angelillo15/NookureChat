7ff4e8d9293618fc6f3f4f7c6fda9990f6cdd3e4
Angelillo15 <contact@angelillo15.es>
2023-12-29 22:23:53 +0100

    Create Paper impl for PlayerJoinLeaveEvent

---
+++ NookureChat-Paper/src/main/java/com/nookure/chat/paper/listeners/PlayerJoinLeaveEvent.java
@@ -0,0 +1,39 @@
+package com.nookure.chat.paper.listeners;
+
+import com.google.inject.Inject;
+import com.nookure.chat.api.config.ConfigurationContainer;
+import com.nookure.chat.api.config.FormatConfig;
+import net.kyori.adventure.title.Title;
+import org.bukkit.entity.Player;
+import org.bukkit.event.EventHandler;
+import org.bukkit.event.Listener;
+import org.bukkit.event.player.PlayerJoinEvent;
+import org.bukkit.event.player.PlayerQuitEvent;
+
+public class PlayerJoinLeaveEvent extends CommonPlayerJoinLeaveEvent implements Listener {
+  @Inject
+  private ConfigurationContainer<FormatConfig> formatConfig;
+
+  @EventHandler
+  public void onPlayerJoin(PlayerJoinEvent event) {
+    CommonPlayerJoinLeaveEvent.Response format = format(event.getPlayer());
+
+    event.joinMessage(format.joinMessage());
+
+    if (formatConfig.get().isEnableJoinTitles()) {
+      Player player = event.getPlayer();
+
+      Title title = Title.title(
+          format.titleJoinMessage(),
+          format.subtitleJoinMessage()
+      );
+
+      player.showTitle(title);
+    }
+  }
+
+  @EventHandler
+  public void onPlayerLeave(PlayerQuitEvent event) {
+    event.quitMessage(format(event.getPlayer()).quitMessage());
+  }
+}
