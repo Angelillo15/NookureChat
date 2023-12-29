fb007187db8e68490cb09d522f98ceffc00671c4
Angelillo15 <contact@angelillo15.es>
2023-12-29 02:58:21 +0100

    Change config aspect

--- NookureChat-API/src/main/java/com/nookure/chat/api/config/Config.java
+++ NookureChat-API/src/main/java/com/nookure/chat/api/config/Config.java
@@ -1,6 +1,7 @@
 package com.nookure.chat.api.config;
 
 import com.nookure.chat.api.config.partials.FloodConfig;
+import com.nookure.chat.api.config.partials.SpamConfig;
 import org.spongepowered.configurate.objectmapping.ConfigSerializable;
 import org.spongepowered.configurate.objectmapping.meta.Setting;
 
@@ -10,8 +11,15 @@ public class Config {
   private boolean debug = false;
   @Setting
   private String prefix = "<b><red>Chat</red></b> <b><gray>»</gray></b>";
-  @Setting
-  public FloodConfig flood = new FloodConfig();
+  public Filters filters = new Filters();
+
+  @ConfigSerializable
+  public static final class Filters {
+    @Setting
+    public FloodConfig flood = new FloodConfig();
+    @Setting
+    public SpamConfig spam = new SpamConfig();
+  }
 
   public String getPrefix() {
     return prefix;
