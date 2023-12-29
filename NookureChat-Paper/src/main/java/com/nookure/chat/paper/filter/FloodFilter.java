3c8eb33534efa44cbaaa5007226d6732681c01a7
Angelillo15 <contact@angelillo15.es>
2023-12-29 19:24:16 +0100

    Create matcher to regex in FloodFilter

--- NookureChat-Paper/src/main/java/com/nookure/chat/paper/filter/FloodFilter.java
+++ NookureChat-Paper/src/main/java/com/nookure/chat/paper/filter/FloodFilter.java
@@ -7,6 +7,8 @@ import com.nookure.chat.api.config.Config;
 import org.bukkit.entity.Player;
 import org.jetbrains.annotations.NotNull;
 
+import java.util.regex.Pattern;
+
 import static com.nookure.chat.paper.utils.MessageUtils.sendMessage;
 
 
@@ -20,8 +22,10 @@ public class FloodFilter extends ChatFilter {
 
   @Override
   public boolean check(@NotNull Player player, @NotNull String message) {
-    if (message.matches("(.)\\1{" + config.flood.getMaxRepetitions() +",}")) {
-      sendMessage(player, config.flood.getDenyMessage());
+    Pattern matcher = Pattern.compile("(.)\\1{" + config.filters.flood.getMaxRepetitions() + ",}");
+
+    if (matcher.matcher(message).find()) {
+      sendMessage(player, config.filters.flood.getDenyMessage());
       return false;
     }
 
