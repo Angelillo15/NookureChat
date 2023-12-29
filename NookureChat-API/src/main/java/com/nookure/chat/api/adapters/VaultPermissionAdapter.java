3db55c8012ece69e663a9ba10db8ab040192ea96
Angelillo15 <contact@angelillo15.es>
2023-12-29 21:52:07 +0100

    Move the setupPermissions to the Adapter

--- NookureChat-API/src/main/java/com/nookure/chat/api/adapters/VaultPermissionAdapter.java
+++ NookureChat-API/src/main/java/com/nookure/chat/api/adapters/VaultPermissionAdapter.java
@@ -1,12 +1,14 @@
 package com.nookure.chat.api.adapters;
 
 import net.milkbowl.vault.permission.Permission;
+import org.bukkit.Bukkit;
 import org.bukkit.entity.Player;
+import org.bukkit.plugin.RegisteredServiceProvider;
 
 public class VaultPermissionAdapter extends PermissionAdapter {
   private final Permission permission;
-  public VaultPermissionAdapter(Permission permission) {
-    this.permission = permission;
+  public VaultPermissionAdapter() {
+    this.permission = setupPermissions();
   }
   @Override
   public String getHighestGroup(Player player) {
@@ -16,5 +18,10 @@ public class VaultPermissionAdapter extends PermissionAdapter {
   @Override
   public String[] getGroups(Player player) {
     return permission.getPlayerGroups(player);
+  }
+
+  private Permission setupPermissions() {
+    RegisteredServiceProvider<Permission> rsp = Bukkit.getServer().getServicesManager().getRegistration(Permission.class);
+    return rsp != null ? rsp.getProvider() : null;
   }
 }
