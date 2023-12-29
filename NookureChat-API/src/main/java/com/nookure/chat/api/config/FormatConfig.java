644b08834f32340a9254f78abdad3ee499431814
Angelillo15 <contact@angelillo15.es>
2023-12-29 22:24:27 +0100

    Add new config fields

--- NookureChat-API/src/main/java/com/nookure/chat/api/config/FormatConfig.java
+++ NookureChat-API/src/main/java/com/nookure/chat/api/config/FormatConfig.java
@@ -10,32 +10,61 @@ import java.util.Map;
 public class FormatConfig {
   @Setting
   private String defaultPrefix = "";
+  @Setting
+  private String defaultSuffix = "";
+  @Setting
+  private String defaultFormat = "{prefix} <reset>{displayname} <gray><b>»</b></gray> {message}";
+  @Setting
+  private String defaultJoinMessage = "<green><b>+ <reset>{displayname}";
+  @Setting
+  private String defaultQuitMessage = "<red><b>- <reset>{displayname}";
+  @Setting
+  private String defaultTitleJoinMessage = "<red><b>Welcome to <white>Nookure<red>!";
+  @Setting
+  private String defaultSubtitleJoinMessage = "<gray>Enjoy your stay!";
+  @Setting
+  private boolean enableJoinQuitMessages = true;
+  @Setting
+  private boolean enableJoinTitles = true;
+
+  public String getDefaultJoinMessage() {
+    return defaultJoinMessage;
+  }
+
+  public String getDefaultQuitMessage() {
+    return defaultQuitMessage;
+  }
 
   public String getDefaultPrefix() {
     return defaultPrefix;
   }
 
-  @Setting
-  private String defaultSuffix = "";
+  public String getDefaultFormat() {
+    return defaultFormat;
+  }
 
   public String getDefaultSuffix() {
     return defaultSuffix;
   }
 
-  @Setting
-  private String defaultFormat = "{prefix} <reset>{displayname} <gray><b>»</b></gray> {message}";
+  public boolean isEnableJoinQuitMessages() {
+    return enableJoinQuitMessages;
+  }
 
-  public String getDefaultFormat() {
-    return defaultFormat;
+  public boolean isEnableJoinTitles() {
+    return enableJoinTitles;
+  }
+
+  public String getDefaultTitleJoinMessage() {
+    return defaultTitleJoinMessage;
+  }
+
+  public String getDefaultSubtitleJoinMessage() {
+    return defaultSubtitleJoinMessage;
   }
 
   @Setting
-  private Map<String, Group> groups = Map.of(
-      "helper", new Group(),
-      "mod", new Group(),
-      "admin", new Group(),
-      "owner", new Group()
-  );
+  private Map<String, Group> groups = Map.of("helper", new Group(), "mod", new Group(), "admin", new Group(), "owner", new Group());
 
   public Map<String, Group> getGroups() {
     return groups;
@@ -49,22 +78,45 @@ public class FormatConfig {
     @Setting
     private String prefix = "";
 
-    public String getPrefix() {
-      return prefix;
-    }
-
     @Setting
     private String suffix = "";
+    @Setting
+    private String format = "{default}";
+    @Setting
+    private String joinMessage = "{default}";
+    @Setting
+    private String quitMessage = "{default}";
+    @Setting
+    private String titleJoinMessage = "{default}";
+    @Setting
+    private String subtitleJoinMessage = "{default}";
 
     public String getSuffix() {
       return suffix;
     }
 
-    @Setting
-    private String format = "{default}";
+    public String getPrefix() {
+      return prefix;
+    }
 
     public String getFormat() {
       return format;
+    }
+
+    public String getJoinMessage() {
+      return joinMessage;
+    }
+
+    public String getQuitMessage() {
+      return quitMessage;
+    }
+
+    public String getTitleJoinMessage() {
+      return titleJoinMessage;
+    }
+
+    public String getSubtitleJoinMessage() {
+      return subtitleJoinMessage;
     }
   }
 }
