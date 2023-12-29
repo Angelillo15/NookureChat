80c32b1fa992370b4cad1289efd85d942b73d18b
Angelillo15 <contact@angelillo15.es>
2023-12-29 02:57:32 +0100

    Create Spam config

---
+++ NookureChat-API/src/main/java/com/nookure/chat/api/config/partials/SpamConfig.java
@@ -0,0 +1,22 @@
+package com.nookure.chat.api.config.partials;
+
+import org.spongepowered.configurate.objectmapping.ConfigSerializable;
+import org.spongepowered.configurate.objectmapping.meta.Setting;
+
+@ConfigSerializable
+public class SpamConfig extends FilterConfig {
+  @Setting
+  private boolean enabled = true;
+  @Setting
+  private String denyMessage = "{prefix} <red>The domains and IPs are forbidden!</red>";
+
+  @Override
+  public boolean isEnabled() {
+    return enabled;
+  }
+
+  @Override
+  public String getDenyMessage() {
+    return denyMessage;
+  }
+}
