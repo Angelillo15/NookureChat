227c3fe2ec5d0f736e30548477b51388da74a4fe
Angelillo15 <contact@angelillo15.es>
2024-01-06 21:26:27 +0100

    Add static variable VERSION to NookureChat interface

--- NookureChat-API/src/main/java/com/nookure/chat/api/NookureChat.java
+++ NookureChat-API/src/main/java/com/nookure/chat/api/NookureChat.java
@@ -1,11 +1,14 @@
 package com.nookure.chat.api;
 
 import com.google.inject.Injector;
+import org.bukkit.Bukkit;
 
 import java.io.File;
 import java.io.InputStream;
 
 public interface NookureChat {
+  int VERSION = Integer.parseInt(Bukkit.getBukkitVersion().split("-")[0].split("\\.")[1]);
+
   Logger getPLogger();
 
   boolean isDebug();
