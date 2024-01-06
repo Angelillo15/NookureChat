c48d762fbbc0423b24e0b72abc69991ed5f084da
Angelillo15 <contact@angelillo15.es>
2024-01-06 21:27:08 +0100

    Sound depend of the minecraft server version

--- NookureChat-API/src/main/java/com/nookure/chat/api/config/partials/broadcast/SoundPartial.java
+++ NookureChat-API/src/main/java/com/nookure/chat/api/config/partials/broadcast/SoundPartial.java
@@ -1,15 +1,25 @@
 package com.nookure.chat.api.config.partials.broadcast;
 
+import com.nookure.chat.api.NookureChat;
 import org.bukkit.Sound;
 import org.spongepowered.configurate.objectmapping.ConfigSerializable;
 import org.spongepowered.configurate.objectmapping.meta.Setting;
 
 @ConfigSerializable
 public class SoundPartial {
+  private static final String SOUND;
+  static {
+    if (NookureChat.VERSION < 13) {
+      SOUND = "LEVEL_UP";
+    } else {
+      SOUND = "ENTITY_PLAYER_LEVELUP";
+    }
+  }
+
   @Setting
   private boolean enabled = true;
   @Setting
-  private Sound sound = Sound.ENTITY_PLAYER_LEVELUP;
+  private Sound sound = Sound.valueOf(SOUND);
   @Setting
   private float volume = 1.0F;
   @Setting
