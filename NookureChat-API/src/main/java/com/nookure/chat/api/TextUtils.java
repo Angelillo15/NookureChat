ef306f4e6cf0b192d96133622dddb0bc84c7297b
Angelillo15 <contact@angelillo15.es>
2024-01-06 20:53:05 +0100

    Create To minimessage method

--- NookureChat-API/src/main/java/com/nookure/chat/api/TextUtils.java
+++ NookureChat-API/src/main/java/com/nookure/chat/api/TextUtils.java
@@ -29,4 +29,33 @@ public class TextUtils {
 
     return message;
   }
+
+  public static String toMM(String str) {
+    if (!str.contains("&") && !str.contains("§")) return str;
+
+    return str
+        .replace("§", "&")
+        .replace("&0", "<reset><black>")
+        .replace("&1", "<reset><dark_blue>")
+        .replace("&2", "<reset><dark_green>")
+        .replace("&3", "<reset><dark_aqua>")
+        .replace("&4", "<reset><dark_red>")
+        .replace("&5", "<reset><dark_purple>")
+        .replace("&6", "<reset><gold>")
+        .replace("&7", "<reset><grey>")
+        .replace("&8", "<reset><dark_grey>")
+        .replace("&9", "<reset><blue>")
+        .replace("&a", "<reset><green>")
+        .replace("&b", "<reset><aqua>")
+        .replace("&c", "<reset><red>")
+        .replace("&d", "<reset><light_purple>")
+        .replace("&e", "<reset><yellow>")
+        .replace("&f", "<reset><white>")
+        .replace("&k", "<obf>")
+        .replace("&l", "<b>")
+        .replace("&m", "<st>")
+        .replace("&n", "<u>")
+        .replace("&r", "<reset>")
+        .replace("&o", "<i>");
+  }
 }
