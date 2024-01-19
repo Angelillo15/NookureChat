ea809f19ff96b571448796019d08b37499c22cd2
Angelillo15 <contact@angelillo15.es>
2024-01-19 16:57:22 +0100

    Create RepeatedMessageFilter

---
+++ NookureChat-Paper/src/main/java/com/nookure/chat/paper/filter/RepeatedMessageFilter.java
@@ -0,0 +1,73 @@
+package com.nookure.chat.paper.filter;
+
+import com.google.inject.Inject;
+import com.nookure.chat.api.chat.ChatFilter;
+import com.nookure.chat.api.chat.ChatFilterData;
+import com.nookure.chat.api.chat.ChatFilterPriority;
+import com.nookure.chat.api.config.Config;
+import com.nookure.chat.api.config.ConfigurationContainer;
+import org.bukkit.entity.Player;
+import org.jetbrains.annotations.NotNull;
+
+import java.util.HashMap;
+import java.util.List;
+import java.util.UUID;
+
+import static com.nookure.chat.paper.utils.MessageUtils.sendMessage;
+
+@ChatFilterData(
+    name = "repeatedMessage",
+    permission = "nookurechat.bypass.filter.repeatedMessage",
+    priority = ChatFilterPriority.FIRST
+)
+public class RepeatedMessageFilter extends ChatFilter {
+  @Inject
+  private ConfigurationContainer<Config> config;
+
+  private final HashMap<UUID, List<Message>> lastMessageMap = new HashMap<>();
+
+  @Override
+  public boolean check(@NotNull Player player, @NotNull String message) {
+    if (!lastMessageMap.containsKey(player.getUniqueId())) {
+      lastMessageMap.put(player.getUniqueId(), List.of(new Message(message, System.currentTimeMillis())));
+      return true;
+    }
+
+    List<Message> lastMessages = lastMessageMap.get(player.getUniqueId());
+
+    lastMessages.forEach(m -> {
+      if (System.currentTimeMillis() - m.time() > config.get().filters.repeatedMessage.getTimeRestriction() * 1000L) {
+        lastMessages.remove(m);
+      }
+    });
+
+    for (Message m : lastMessages) {
+      if (m.message().equals(message)) {
+        sendMessage(player, config.get().filters.repeatedMessage.getDenyMessage()
+            .replace("{time}", String.valueOf(
+                config.get().filters.repeatedMessage.getTimeRestriction() - (System.currentTimeMillis() - m.time()) / 1000L)
+            )
+        );
+        return false;
+      }
+    }
+
+    lastMessages.add(new Message(message, System.currentTimeMillis()));
+    lastMessageMap.put(player.getUniqueId(), lastMessages);
+
+    return true;
+  }
+
+  private record Message(String message, long time) {
+    @Override
+    public boolean equals(Object obj) {
+      if (obj instanceof String) {
+        return message.equals(obj);
+      } else if (obj instanceof Message) {
+        return message.equals(((Message) obj).message());
+      } else {
+        return false;
+      }
+    }
+  }
+}
