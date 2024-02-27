b57e03959ceda72df2f8d3ed008fc7f20e27c685
Angelillo15 <contact@angelillo15.es>
2024-02-27 17:10:05 +0100

    Shade bstats

--- NookureChat-Paper/build.gradle.kts
+++ NookureChat-Paper/build.gradle.kts
@@ -1,20 +1,25 @@
 plugins {
-    id("net.minecrell.plugin-yml.bukkit") version "0.6.0"
+  id("net.minecrell.plugin-yml.bukkit") version "0.6.0"
 }
 
 dependencies {
-    compileOnly(project(":NookureChat-API"))
-    compileOnly(libs.paperApi)
-    compileOnly(libs.bundles.adventure)
-    compileOnly(libs.vault)
+  compileOnly(project(":NookureChat-API"))
+  compileOnly(libs.paperApi)
+  compileOnly(libs.bundles.adventure)
+  compileOnly(libs.vault)
+  implementation(libs.bstatsBukkit)
+}
+
+tasks.shadowJar {
+  relocate("org.bstats", "com.nookure.chat.libs.bstats")
 }
 
 bukkit {
-    name = "NookureChat"
-    main = "com.nookure.chat.paper.bootstrap.ChatBootstrapper"
-    apiVersion = "1.13"
-    version = rootProject.version.toString()
-    description = "A chat plugin for Minecraft"
-    website = "https://nookure.com"
-    authors = listOf("Angelillo15", "Nookure Contributors")
+  name = "NookureChat"
+  main = "com.nookure.chat.paper.bootstrap.ChatBootstrapper"
+  apiVersion = "1.13"
+  version = rootProject.version.toString()
+  description = "A chat plugin for Minecraft"
+  website = "https://nookure.com"
+  authors = listOf("Angelillo15", "Nookure Contributors")
 }
