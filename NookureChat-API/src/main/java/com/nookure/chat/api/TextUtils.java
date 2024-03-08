64bd28bda61d1f5dd1a47ccba216fcb2aafb24f0
Hugo Torres <46359630+Reddishye@users.noreply.github.com>
2024-03-08 23:43:59 +0100

    Reviewed changes requested by Angelillo15.

--- NookureChat-API/src/main/java/com/nookure/chat/api/TextUtils.java
+++ NookureChat-API/src/main/java/com/nookure/chat/api/TextUtils.java
@@ -2,12 +2,12 @@ package com.nookure.chat.api;
 
 import me.clip.placeholderapi.PlaceholderAPI;
 import org.bukkit.entity.Player;
-
-import java.util.regex.Matcher;
 import java.util.regex.Pattern;
+import java.util.regex.Matcher;
 
 public class TextUtils {
   private static final boolean isPlaceholderAPI;
+  private static final Pattern hexPattern = Pattern.compile("&#([A-Fa-f0-9]{6})");
 
   static {
     boolean isPlaceholderAPITemp;
@@ -17,7 +17,6 @@ public class TextUtils {
     } catch (ClassNotFoundException e) {
       isPlaceholderAPITemp = false;
     }
-
     isPlaceholderAPI = isPlaceholderAPITemp;
   }
 
@@ -29,48 +28,45 @@ public class TextUtils {
     } catch (Exception e) {
       return message;
     }
-
     return message;
   }
 
   public static String toMM(String str) {
-    if (!str.contains("&") && !str.contains("§")) return str;
+    str = str.replace("§", "&")
+      .replace("&0", "<black>")
+      .replace("&1", "<dark_blue>")
+      .replace("&2", "<dark_green>")
+      .replace("&3", "<dark_aqua>")
+      .replace("&4", "<dark_red>")
+      .replace("&5", "<dark_purple>")
+      .replace("&6", "<gold>")
+      .replace("&7", "<grey>")
+      .replace("&8", "<dark_grey>")
+      .replace("&9", "<blue>")
+      .replace("&a", "<green>")
+      .replace("&b", "<aqua>")
+      .replace("&c", "<red>")
+      .replace("&d", "<light_purple>")
+      .replace("&e", "<yellow>")
+      .replace("&f", "<white>")
+      .replace("&k", "<obf>")
+      .replace("&l", "<b>")
+      .replace("&m", "<st>")
+      .replace("&n", "<u>")
+      .replace("&o", "<i>")
+      .replace("&r", "<reset>");
 
-    str = str
-            .replace("§", "&")
-            .replace("&0", "<black>")
-            .replace("&1", "<dark_blue>")
-            .replace("&2", "<dark_green>")
-            .replace("&3", "<dark_aqua>")
-            .replace("&4", "<dark_red>")
-            .replace("&5", "<dark_purple>")
-            .replace("&6", "<gold>")
-            .replace("&7", "<grey>")
-            .replace("&8", "<dark_grey>")
-            .replace("&9", "<blue>")
-            .replace("&a", "<green>")
-            .replace("&b", "<aqua>")
-            .replace("&c", "<red>")
-            .replace("&d", "<light_purple>")
-            .replace("&e", "<yellow>")
-            .replace("&f", "<white>")
-            .replace("&k", "<obf>")
-            .replace("&l", "<b>")
-            .replace("&m", "<st>")
-            .replace("&n", "<u>")
-            .replace("&o", "<i>")
-            .replace("&r", "<reset>");
-
-    Pattern hexPattern = Pattern.compile("&#([A-Fa-f0-9]{6})");
-    Matcher matcher = hexPattern.matcher(str);
-
-    StringBuilder buffer = new StringBuilder();
-    while (matcher.find()) {
-      String replacement = String.format("<#%s>", matcher.group(1));
-      matcher.appendReplacement(buffer, replacement);
+    if (str.contains("#")) {
+      Matcher matcher = hexPattern.matcher(str);
+      StringBuilder buffer = new StringBuilder();
+      while (matcher.find()) {
+        String replacement = String.format("<#%s>", matcher.group(1));
+        matcher.appendReplacement(buffer, replacement);
+      }
+      matcher.appendTail(buffer);
+      str = buffer.toString();
     }
-    matcher.appendTail(buffer);
 
-    return buffer.toString();
+    return str;
   }
 }
