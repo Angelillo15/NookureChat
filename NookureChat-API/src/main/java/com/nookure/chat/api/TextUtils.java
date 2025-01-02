2910aacc68c4abd7cc96706c488126fa5c6e69c4
Angelillo15 <contact@angelillo15.es>
2025-01-02 17:34:17 +0100

    feat: add more tags and invert legacy format

--- NookureChat-API/src/main/java/com/nookure/chat/api/TextUtils.java
+++ NookureChat-API/src/main/java/com/nookure/chat/api/TextUtils.java
@@ -19,7 +19,10 @@ public class TextUtils {
       .tags(TagResolver.builder()
           .resolvers(
               StandardTags.color(),
-              StandardTags.decorations()
+              StandardTags.decorations(),
+              StandardTags.rainbow(),
+              StandardTags.gradient(),
+              StandardTags.pride()
           )
           .build())
       .build();
@@ -47,29 +50,29 @@ public class TextUtils {
   }
 
   public static String toMM(@NotNull String str) {
-    str = str.replace("§", "&")
-        .replace("&0", "<black>")
-        .replace("&1", "<dark_blue>")
-        .replace("&2", "<dark_green>")
-        .replace("&3", "<dark_aqua>")
-        .replace("&4", "<dark_red>")
-        .replace("&5", "<dark_purple>")
-        .replace("&6", "<gold>")
-        .replace("&7", "<grey>")
-        .replace("&8", "<dark_grey>")
-        .replace("&9", "<blue>")
-        .replace("&a", "<green>")
-        .replace("&b", "<aqua>")
-        .replace("&c", "<red>")
-        .replace("&d", "<light_purple>")
-        .replace("&e", "<yellow>")
-        .replace("&f", "<white>")
-        .replace("&k", "<obf>")
-        .replace("&l", "<b>")
-        .replace("&m", "<st>")
-        .replace("&n", "<u>")
-        .replace("&o", "<i>")
-        .replace("&r", "<reset>");
+    str = str.replace("&", "§")
+        .replace("§0", "<black>")
+        .replace("§1", "<dark_blue>")
+        .replace("§2", "<dark_green>")
+        .replace("§3", "<dark_aqua>")
+        .replace("§4", "<dark_red>")
+        .replace("§5", "<dark_purple>")
+        .replace("§6", "<gold>")
+        .replace("§7", "<grey>")
+        .replace("§8", "<dark_grey>")
+        .replace("§9", "<blue>")
+        .replace("§a", "<green>")
+        .replace("§b", "<aqua>")
+        .replace("§c", "<red>")
+        .replace("§d", "<light_purple>")
+        .replace("§e", "<yellow>")
+        .replace("§f", "<white>")
+        .replace("§k", "<obf>")
+        .replace("§l", "<b>")
+        .replace("§m", "<st>")
+        .replace("§n", "<u>")
+        .replace("§o", "<i>")
+        .replace("§r", "<reset>");
 
     if (str.contains("#")) {
       Matcher matcher = hexPattern.matcher(str);
